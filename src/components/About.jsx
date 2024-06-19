879ddf57fdcb9c26a51c867a693b80954a945ce7
GabrielAsis <113765432+GabrielAsis@users.noreply.github.com>
2024-06-19 18:03:51 +0400

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,10 @@ import Section from "./Section"
 const About = () => {
   return (
     <Section bottomLine={true}>
-      <div className="container">
-        ABOUT
+      <div className="container py-8">
+        <h1 className="font-roboto font-bold text-3xl">
+          About- <span className="italic">get to know me</span>
+        </h1>
       </div>
     </Section>
   )
